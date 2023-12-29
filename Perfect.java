03f6b3f1a1273957d756e574e52c843400698fa6
shirshani1 <shir.shani@post.runi.ac.il>
2023-12-29 17:43:05 +0200

    assignment

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,22 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
-	}
+		int sum = 1;
+		int num= Integer.parseInt(args[0]);
+		String printed = (num + " is a perfect number since " + num + " = 1"); 
+		for (int i = 2; i< num; i++) {
+			if (num%i == 0) {
+				sum =+ i;
+				// System.out.println(i);
+				printed += (" + " + i);}
+			}
+		// System.out.println(sum);
+		if (num == sum) {
+			System.out.println(printed);
+			System.out.println(sum);
+			} else {
+				System.out.println(num + " is not a perfect number");
+				}
+		}
+		
 }
