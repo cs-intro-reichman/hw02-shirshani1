7664ae22c97dcc325a18c75a7b148d8ee148e820
shirshani1 <shir.shani@post.runi.ac.il>
2023-12-29 17:08:11 +0200

    assigment

--- InOrder.java
+++ InOrder.java
@@ -4,6 +4,14 @@
  */
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+		int firstNum= ((int)(Math.random() * (10)));
+		System.out.print(firstNum);
+		int secNum = 0;
+		do {
+			secNum= ((int)(Math.random() * (10)));
+			if (secNum > firstNum) {
+				System.out.print(" " + secNum);}
+			firstNum = secNum;
+		} while (secNum > secNum);
 	}
 }
