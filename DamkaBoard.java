1d704b4c33bc7394018092d48e58d15d0f227216
shirshani1 <shir.shani@post.runi.ac.il>
2023-12-30 17:45:01 +0200

    assidnment

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,20 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+
+		int numLines = Integer.parseInt(args[0]);
+		for (int i = 1;i <= numLines ;i++ ) {
+				if (i % 2 == 0) {
+						System.out.print("  *");
+				} else { System.out.print(" *");}
+				for (int j = 1 ;j <= numLines ;j++) {
+						System.out.print(" *");
+			}
+			System.out.println(" ");
+		 }	
+			
+		}
 	}
-}
+
+	
+
