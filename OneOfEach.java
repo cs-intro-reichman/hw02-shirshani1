0dd54c0b72a225793ec8766647ec4392447a654c
shirshani1 <shir.shani@post.runi.ac.il>
2024-01-01 21:42:14 +0200

    assignment

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,25 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		boolean girl = false; //at list one girl
+		boolean boy = false; //at list one girl
+		int numChildren = 0;
+		while (girl == false || boy == false) {
+			double result = Math.random();
+			// System.out.println(result);
+			if (result > 0.5) {
+				girl = true;
+				System.out.print("g ");
+				numChildren += 1;
+			} else {
+				boy = true;
+				System.out.print("b ");
+				numChildren += 1;
+
+			}
+			
+		}
+		System.out.println();
+		System.out.println("You made it... and you now have " + numChildren + " children."); 
 	}
 }
