03632c89cc7d994d5e52c90ab4bb1d4e6ccdb864
shirshani1 <shir.shani@post.runi.ac.il>
2024-01-03 22:22:44 +0200

    assignment

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -13,7 +13,50 @@ public class OneOfEachStats {
 		int T = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
-        Random generator = new Random(seed);  
+        Random generator = new Random(seed); 
+        double sumChildren = 0;
+		int num2Children = 0;
+		int num3Children = 0;
+		int numManyChildren =0;
+		for (int i = 1;i < T ;i++) {
+			boolean girl = false; //at list one girl
+			boolean boy = false; //at list one girl
+			int numChildren = 0;
+			while (girl == false || boy == false) {
+				double result = generator.nextDouble();
+				// System.out.println(result);
+				if (result > 0.5) {
+					girl = true;
+					numChildren += 1;
+				} else {
+					boy = true;
+					numChildren += 1;
+				}
+				if (numChildren == 2) {
+					num2Children += 1;
+				}else if (numChildren == 3) {
+					num3Children += 1;
+				}else if (numChildren > 3) {
+					numManyChildren += 1;
+					}
+				}
+				sumChildren += numChildren;
+			}
+		double avreage = sumChildren/ T;
+		System.out.println("Average: " + avreage + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + num2Children);
+		System.out.println("Number of families with 3 children: " + num3Children);
+		System.out.println("Number of families with 4 or more children: " + numManyChildren);
+		if (num2Children > num3Children && num2Children > numManyChildren) {
+			 System.out.println("The most common number of children is 2.");
+		}else if (num3Children > num2Children && num3Children > numManyChildren) {
+			 System.out.println("The most common number of children is 3.");
+		}else if (numManyChildren > num2Children && numManyChildren > num3Children) {
+			 System.out.println("The most common number of children is 4 or more.");
+			}
+	}
+}
+ 
 		
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
@@ -25,5 +68,5 @@ public class OneOfEachStats {
 		//// randomization will be based on the given seed.
 		//// This is the only change that you have to do in the program.
 		    
-	}
-}
+	
+
--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -6,6 +6,46 @@
  */
 public class OneOfEachStats1 {
 	public static void main (String[] args) {
-		//// Put your code here
+		int T = Integer.parseInt(args[0]);
+		double sumChildren = 0;
+		int num2Children = 0;
+		int num3Children = 0;
+		int numManyChildren =0;
+		for (int i = 1;i < T ;i++) {
+			boolean girl = false; //at list one girl
+			boolean boy = false; //at list one girl
+			int numChildren = 0;
+			while (girl == false || boy == false) {
+				double result = Math.random();
+				// System.out.println(result);
+				if (result > 0.5) {
+					girl = true;
+					numChildren += 1;
+				} else {
+					boy = true;
+					numChildren += 1;
+				}
+				if (numChildren == 2) {
+					num2Children += 1;
+				}else if (numChildren == 3) {
+					num3Children += 1;
+				}else if (numChildren > 3) {
+					numManyChildren += 1;
+					}
+				}
+				sumChildren += numChildren;
+			}
+		double avreage = sumChildren/ T;
+		System.out.println("Average: " + avreage + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + num2Children);
+		System.out.println("Number of families with 3 children: " + num3Children);
+		System.out.println("Number of families with 4 or more children: " + numManyChildren);
+		if (num2Children > num3Children && num2Children > numManyChildren) {
+			 System.out.println("The most common number of children is 2.");
+		}else if (num3Children > num2Children && num3Children > numManyChildren) {
+			 System.out.println("The most common number of children is 3.");
+		}else if (numManyChildren > num2Children && numManyChildren > num3Children) {
+			 System.out.println("The most common number of children is 4 or more.");
+			}
 	}
 }
